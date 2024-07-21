c018f535a946573c1876b4dceff14e2576cfdb59
kcho-adri <aed.polanco@gmail.com>
2024-07-20 22:34:41 -0300

    Se agregó el SoftDeletes al Modelo

--- Prueba_Programacion/app/Http/Controllers/PersonaController.php
+++ Prueba_Programacion/app/Http/Controllers/PersonaController.php
@@ -7,22 +7,8 @@ use Illuminate\Http\Request;
 
 class PersonaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function Altas()
     {
         //
     }
